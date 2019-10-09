8f52996e61ffa0490adc38b059e6e92c76dd83ce
Rohan Garg <rohangarg12@gmail.com>
2019-10-09 22:19:38 +0530

    corrected for multiple seeds

--- seed.py
+++ seed.py
@@ -11,30 +11,38 @@ import random
 seed_sockets = []
 local_clients = []
 all_clients = []
+stop_seeds = 0
 
 def seed_receive_thread(c):
 	global all_clients
 	global local_clients
+	global stop_seeds
+
 	data = ""
-	while True: 
-		print("data received")
-		data = c.recv(1024) 
-		if not data: 
-			break
+	while True:
+		while True: 
+			data = c.recv(1024) 
+			if not data: 
+				break
+			print("data received: "+data.decode('ascii'))
+		
+		if(data.decode('ascii') == "no seeds"):
+			stop_seeds = 1
+			continue
 
-	new_client = data.decode('ascii').split(':')
+		new_client = data.decode('ascii').split(':')
 
-	## if check_client stays 0, this means that this client is already known by this seed
-	check_client = 0
-	for i in all_clients:
-		if i==new_client:
-			check_ml = 1
-			break
-	
-	if check_ml ==0:
-		all_clients.append(new_client)
-		thread = threading.Thread(target=threaded_client, args=(new_client,))
-		thread.start()
+		## if check_client stays 0, this means that this client is already known by this seed
+		check_client = 0
+		for i in all_clients:
+			if i==new_client:
+				check_client = 1
+				break
+		
+		if check_client ==0:
+			all_clients.append(new_client)
+			thread = threading.Thread(target=threaded_client, args=(new_client,))
+			thread.start()
 
 def threaded_client(addr):
 	global seed_sockets
@@ -74,14 +82,16 @@ def listen_client_thread():
 def listen_seed_thread():
 	global listening_port
 	global seed_sockets
-	host = ""
+	global stop_seeds
+
 	port = listening_port
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((host, port))
+	s.bind(('', port))
 	s.listen()
+	print("Listening for incoming connections")
 	timeout = time.time() + 60*2
-	while True:
+	while (stop_seeds == 0):
 		## accept incoming connections
 		c, addr = s.accept()
 		# print("cnct_acc")
@@ -94,29 +104,28 @@ def listen_seed_thread():
 			break
 	s.close()
 
-def Main():
-	global listening_port
-	global seed_sockets
-	global local_clients
+## total number of seed nodes as of now (including this one)
+print("Hello")
+n = int(sys.argv[1])
+listening_port = int(sys.argv[2])
+print(n)
+k=3
+seeds_info =[] 
 
-	## total number of seed nodes as of now (including this one)
-	n = int(sys.argv[1])
-	listening_port = int(sys.argv[2])
-	print(n)
-	k=3
-	seeds_info =[] 
+for i in range(n-1):
+	seeds_info.append([sys.argv[k],sys.argv[k+1]])
+	k = k+2
+print(seeds_info)
 
-	for i in range(n-1):
-		seeds_info.append([sys.argv[k],sys.argv[k+1]])
-		k = k+2
-	print(seeds_info)
-	
-	# s.bind(('', listening_port))
-	thread = threading.Thread(target=listen_seed_thread, args=(listening_port))
+last_seed = int(sys.argv[k])
+
+neighbours = random.choices(seeds_info, k=random.choice(list(range(1,len(seeds_info)+1)) if len(seeds_info)>0 else [0]))
+
+# s.bind(('', listening_port))
+if(last_seed == 0):
+	thread = threading.Thread(target=listen_seed_thread, args=())
 	thread.start()
-	
-	neighbours = random.choices(seeds_info, k=random.choice(list(range(1,len(seeds_info)+1)) if len(seeds_info)>0 else [0]))
-	
+
 	for node in neighbours:
 		host = node[0]
 		port = int(node[1])
@@ -126,8 +135,20 @@ def Main():
 		recieve_data = threading.Thread(target=seed_receive_thread, args=(s,))
 		recieve_data.start()
 
-	thread_client = threading.Thread(target=listen_client_thread, args=())
-	thread_client.start()
+	thread.join()
 
-if _name_ == '_main_': 
-	Main()
+else:
+	for node in neighbours:
+		host = node[0]
+		port = int(node[1])
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host, port))
+		seed_sockets.append(s)
+		recieve_data = threading.Thread(target=seed_receive_thread, args=(s,))
+		recieve_data.start()
+
+	for sock in seed_sockets:
+		sock.send("no seeds".encode('ascii'))
+
+thread_client = threading.Thread(target=listen_client_thread, args=())
+thread_client.start()
